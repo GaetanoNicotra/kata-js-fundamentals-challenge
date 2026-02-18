2c12f45166332f08c5217eda053a6937b3267e78
GaetanoNicotra <nicotragaetano10@gmail.com>
2026-02-18 18:59:10 +0100

    [UPD] esercizi 06 completati

--- src/06-array-methods.js
+++ src/06-array-methods.js
@@ -2,13 +2,13 @@
 
 // 1. Use map to create a new array with the numbers doubled
 function doubleNumbers(arr) {
-  // write your solution here
-}
+  return arr.map(a => a * 2);
+};
 
 // 2. Use filter to return only even numbers
 function filterEven(arr) {
-  // write your solution here
-}
+  return arr.filter(a => a % 2 === 0);
+};
 
 module.exports = {
   doubleNumbers,
