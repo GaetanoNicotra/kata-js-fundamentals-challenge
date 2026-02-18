6621aed205607512683b9c1702b5eb87fe96527c
GaetanoNicotra <nicotragaetano10@gmail.com>
2026-02-18 18:47:06 +0100

    [UPD] esercizi 02 completati

--- src/02-for-loop-and-arrays.js
+++ src/02-for-loop-and-arrays.js
@@ -7,16 +7,36 @@ let word = '' // eslint-disable-line prefer-const
 
 // 1. Use a for loop to set the sum variable to the sum of all the values in nums
 
+for (let i = 0; i < nums.length; i++) {
+  sum += nums[i];
+}
+
 // 2. Use a for loop to populate doubledNums with every value from nums array doubled (i.e [2, 6, 24, etc...])
 const doubledNums = []
 
+for (let i = 0; i < nums.length; i++) {
+  double = nums[i] * 2;
+  doubledNums.push(double)
+}
+
 // 3. Use a for loop to set word equal to all the letters in the letters array
+
+word = letters.join('');
 
 // 4. Use a for loop to populate everySecondNum with every second number from the nums array
 const everySecondNum = []
 
+for (let i = 1; i < nums.length; i += 2) {
+  everySecondNum.push(nums[i]);
+}
+
+
 // 5. Use a for loop to populate numsReversed with the numbers from nums in reverse order
 const numsReversed = []
+
+for (let i = nums.length - 1; i >= 0; i--) {
+  numsReversed.push(nums[i]);
+}
 
 // do not change below this line
 module.exports = {
@@ -26,3 +46,4 @@ module.exports = {
   d: everySecondNum,
   e: numsReversed
 }
+
